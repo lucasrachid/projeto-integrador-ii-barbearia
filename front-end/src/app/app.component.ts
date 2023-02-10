721bf3e67c32b503a2429330bc12a8fc43255df7
Lucas Rachid Martins <softfocus@nextage.com.br>
2023-02-10 15:55:09 -0300

    Correção Internacionalização.

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InternationalizationService } from './components/internationalization.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Barbearia';
+
+  constructor(private internationalizationService: InternationalizationService) {
+    internationalizationService.initializeSystemTranslate();
+  }
 }
